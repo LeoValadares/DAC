beac32949f289a4d508e3b2c73ed6577a76f2103
Leonardo Valadares <leovaladares@Leonardos-MacBook-Pro.local>
2018-04-02 15:52:52 -0300

    exercício 3 ok

--- Tarefa1/Exercicio3/index.js
+++ Tarefa1/Exercicio3/index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
 const session = require('express-session');
+const cookieParser = require('cookie-parser');
 
 const app = express();
-
 
 //setup do handlebars
 app.engine('handlebars', exphbs());
@@ -12,11 +12,16 @@ app.set("view engine", "handlebars");
 //setupu do session
 app.use(session({
     secret: 'my express secret',
-  }));
+}));
+app.use(cookieParser());
 
+//contador de acessos totais, fica na memória do servidor
+let totalAccesses = 0;
 
 //Configuração de rotas
 app.get("/tarefa1/somatorio", (req, res, next) => {
+    totalAccesses++;
+    
     let soma = 0;
     let inicio = parseInt(req.query['inicio']);
     let fim = parseInt(req.query['fim']);
@@ -24,16 +29,22 @@ app.get("/tarefa1/somatorio", (req, res, next) => {
         soma = soma + i;
     }
 
+    //contagem por sessão
     if (typeof req.session.views === 'undefined') {
         req.session.views = 1;
     }else{
         req.session.views++
     }
 
-    console.log(req.session)
-    views = req.session.views;
+    let browserViews = req.cookies.browserViews;
+    //contagem por browser
+    if (browserViews) browserViews++;
+    else browserViews = 1;
+    res.cookie('browserViews', browserViews, { maxAge: 900000 });
 
-    res.render("Somatorio", { inicio, fim, soma, views });
+    let views = req.session.views;
+
+    res.render("Somatorio", { inicio, fim, soma, views, browserViews, totalAccesses });
 });
 
 app.listen(8080, () => console.log("Server started!!!"));
