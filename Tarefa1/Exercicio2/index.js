8f0786088eb7df075d15e1972edec51d6ba401b4
mateusrangel <mateusrangel@id.uff.br>
2018-04-02 02:12:40 -0300

    exercicio2

---
+++ Tarefa1/Exercicio2/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const exphbs = require("express-handlebars");
+
+//setup do handlebars
+app.engine('handlebars', exphbs());
+app.set("view engine", "handlebars");
+
+//Configuração de rotas
+app.get("/tarefa1/mostraheader", (req, res) => {
+    //pegando os headers com req.headers
+    headers = req.headers;
+    //renderizando com handlebars passando o objeto headers como parametro
+    res.render("MostraHeader", {
+        headers: headers
+    });
+});
+
+app.listen(8082, () => console.log("Server started!!!"));
